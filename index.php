fc65f022bacc693c0df12366b14d5e0aa1eb8b13
FreekAlberti <federicoalberi96@gmail.com>
2020-10-07 16:29:39 +0200

    section

--- index.php
+++ index.php
@@ -19,13 +19,19 @@ include "db.php";
   </header>
 
   <section>
-    <div class="traccia">
-      <?php foreach ($database as $album) { ?>
-        <img src="<?php echo $album["poster"] ?>" alt="Copertina">
-        <h3><?php echo $album["title"] ?></h3>
-        <h4><?php echo $album["author"] ?></h4>
-        <h4><?php echo $album["year"] ?></h4>
-      <?php } ?>
+    <div class="container">
+      <div class="wrapper">
+        <?php foreach ($database as $album) { ?>
+          <div class="traccia">
+            <img class="copertina" src="<?php echo $album["poster"] ?>" alt="Copertina">
+            <div class="testo">
+              <h3><?php echo $album["title"] ?></h3>
+              <h4><?php echo $album["author"] ?></h4>
+              <h4><?php echo $album["year"] ?></h4>
+            </div>
+          </div>
+        <?php } ?>
+      </div>
     </div>
   </section>
 </body>
